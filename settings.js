61a7a4c726862981cfa380a566307df957adc139
Sonnoah <Dllight1031@gmail.com>
2025-09-19 01:17:09 +0700

    admin

--- settings.js
+++ settings.js
@@ -1,15 +1,28 @@
 const path = require("path");
 
 module.exports = {
-  disableEditor: true,
+
+
   httpNodeRoot: "/",
   ui: { path: "/ui" },
-  credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET || "change-this-in-env",
   httpStatic: path.join(__dirname, "public"),
 
-  editorTheme: {
-    projects: { enabled: false }
+
+  adminAuth: {
+    type: "credentials",
+    users: [
+      {
+        username: "admin",
+        password: "$2b$08$PUT_YOUR_BCRYPT_HASH_HERE",
+        permissions: "*"   
+      },
+
+    ]
   },
+
+  editorTheme: {
+    projects: { enabled: false } 
+  },
+
+  credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET || "change-this-in-env",
 };
-
-

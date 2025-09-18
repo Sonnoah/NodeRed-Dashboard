a22737e3407f995744811d75c3bf45ad7a4a2b1b
Sonnoah <Dllight1031@gmail.com>
2025-09-19 01:54:08 +0700

    admin

--- settings.js
+++ settings.js
@@ -1,27 +1,23 @@
 const path = require("path");
 
 module.exports = {
-
-
   httpNodeRoot: "/",
   ui: { path: "/ui" },
   httpStatic: path.join(__dirname, "public"),
-
 
   adminAuth: {
     type: "credentials",
     users: [
       {
         username: "admin",
-        password: "$2b$08$PUT_YOUR_BCRYPT_HASH_HERE",
-        permissions: "*"   
-      },
-
+        password: "admin",
+        permissions: "*"
+      }
     ]
   },
 
   editorTheme: {
-    projects: { enabled: false } 
+    projects: { enabled: false }
   },
 
   credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET || "change-this-in-env",
